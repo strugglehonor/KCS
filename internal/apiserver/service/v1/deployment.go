7679403b9dd91c3b16804480af4ae1721708b113
strugglehonor <2537731609@qq.com>
2022-02-05 11:37:40 +0800

    service mysql crud

--- internal/apiserver/service/v1/deployment.go
+++ internal/apiserver/service/v1/deployment.go
@@ -10,6 +10,7 @@ import (
 type DeploymentSrv interface {
 	Create(ctx context.Context, deployment *model.Deployment) error
 	Delete(ctx context.Context, deployment *model.Deployment) error 
+	Update(ctx context.Context, deployment *model.Deployment) error
 }
 
 type DeploymentService struct {
@@ -24,9 +25,22 @@ func newDeployment(s *service) DeploymentSrv {
 }
 
 func (deploymentService *DeploymentService) Create(ctx context.Context, deployment *model.Deployment) error {
+	if err := deploymentService.store.Deployment().Create(ctx, deployment); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (deploymentService *DeploymentService) Delete(ctx context.Context, deployment *model.Deployment) error {
+	if err := deploymentService.store.Deployment().Delete(ctx, deployment); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (DeploymentService *DeploymentService) Update(ctx context.Context, deployment *model.Deployment) error {
+	if err := DeploymentService.store.Deployment().Update(ctx, deployment); err != nil {
+		return err
+	}
 	return nil
 }
--- internal/apiserver/service/v1/pod.go
+++ internal/apiserver/service/v1/pod.go
@@ -9,7 +9,12 @@ import (
 
 type PodSrv interface {
 	Create(ctx context.Context, pod *model.Pod) error
-	Delete(ctx context.Context)
+	Delete(ctx context.Context, pod *model.Pod) error
+	GetPodByName(ctx context.Context, podName string) (*model.Pod, error) 
+	InsertPod(ctx context.Context, ID, name, namespace, status, creator, restartPolicy,
+		imagePullPolicy, dnsPolicy, ip string, memLimit, cpulimit, gpulimit, lifelimit int32, env map[interface{}]interface{},
+		hostIPC, hostNetwork, hostPID bool, node model.Node, cluster model.Cluster) error
+	Update(ctx context.Context, pod *model.Pod) error
 }
 
 type PodService struct {
@@ -26,10 +31,61 @@ func (podService *PodService) Create(ctx context.Context, pod *model.Pod) error 
 	if err := podService.store.Pod().Create(ctx, pod); err != nil {
 		return err
 	}
-
+	
 	return nil
 }
 
-func (podService *PodService) Delete(ctx context.Context) {
+func (podService *PodService) Delete(ctx context.Context, pod *model.Pod) error {
+	if err := podService.store.Pod().Delete(ctx, pod); err != nil {
+		return err
+	}
+	return nil
+}
 
+func (podService *PodService) GetPodByName(ctx context.Context, podName string) (*model.Pod, error) {
+	pod, err := podService.store.Pod().GetPodByName(ctx, podName)
+	if err != nil {
+		return nil, err
+	} 
+
+	return pod, err
+}
+
+func (podService *PodService) InsertPod(ctx context.Context, ID, name, namespace, status, creator, restartPolicy,
+    imagePullPolicy, dnsPolicy, ip string, memLimit, cpulimit, gpulimit, lifelimit int32, env map[interface{}]interface{},
+    hostIPC, hostNetwork, hostPID bool, node model.Node, cluster model.Cluster) error {
+	
+	pod := model.Pod{
+		ID: ID,
+		Ip: ip,
+		Name: name,
+		NameSpace: namespace,
+		Node: node,
+		Status: status,
+		Cluster: &cluster,
+		Creator: creator,
+		RestartPolicy: restartPolicy,
+		ImagePullPolicy: imagePullPolicy,
+		DNSPolicy: dnsPolicy,
+		MemLimit: memLimit,
+		CpuLimit: cpulimit,
+		GpuLimit: gpulimit,
+		LifeLimit: lifelimit,
+		Env: env,
+		HostIPC: hostIPC,
+		HostNetwork: hostNetwork,
+		HostPID: hostPID,
+	}
+
+	if err := podService.store.Pod().Create(ctx, &pod); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (podService *PodService) Update(ctx context.Context, pod *model.Pod) error {
+	if err := podService.store.Pod().Update(ctx, pod); err != nil {
+		return err
+	} 
+	return nil
 }
